97221a5d3004dc6016861e9340bcfd7f47352fe7
Waseemk1 <Kamran.Waseem@student.greenriver.edu>
2024-10-10 08:48:36 -0700

    Added.hello.py script

---
+++ Print("what is your name").py
@@ -0,0 +1,4 @@
+Print("what is your name")
+print("Hello World")
+print("Today is going to be a great day")
+print("how old are you?")
---
+++ Welcome.py
@@ -0,0 +1,5 @@
+print("Hello and welcome to linus")
+print("test to make sure")
+
+Print("Hello World")
+
